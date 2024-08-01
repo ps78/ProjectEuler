58c427a770b5ce4b2d3b44a267f0c5106d02fc57
Pascal Simon <mail@pascalsimon.com>
2024-08-01 21:48:09 +0700

    formatting

--- ProjectEuler/Unsolved/Problem543.cs
+++ ProjectEuler/Unsolved/Problem543.cs
@@ -10,8 +10,10 @@ namespace ProjectEuler
     /// <summary>
     /// Solves https://projecteuler.net/problem=543
     /// 
-    /// Define function P(n,k) = 1 if n can be written as the sum of k prime numbers (with repetitions allowed), and P(n,k) = 0 otherwise.
-    /// For example, P(10, 2) = 1 because 10 can be written as either 3 + 7 or 5 + 5, but P(11,2) = 0 because no two primes can sum to 11.
+    /// Define function P(n,k) = 1 if n can be written as the sum of k prime numbers (with repetitions allowed), 
+    /// and P(n,k) = 0 otherwise.
+    /// For example, P(10, 2) = 1 because 10 can be written as either 3 + 7 or 5 + 5, 
+    /// but P(11,2) = 0 because no two primes can sum to 11.
     /// Let S(n) be the sum of all P(i, k) over 1 ≤ i,k ≤ n.
     /// For example, S(10) = 20, S(100) = 2402, and S(1000) = 248838.
     /// Let F(k) be the kth Fibonacci number(with F(0) = 0 and F(1) = 1).
@@ -19,12 +21,12 @@ namespace ProjectEuler
     /// </summary>
     public class Problem543 : EulerProblemBase
     {
-        public Problem543() : base(543, "Prime-Sum Numbers", 44) { }
+        public Problem543() : base(543, "Prime-Sum Numbers", 0, 0) { }
 
         public override long Solve(long n)
         {
             long sum = 0;
-            for (long fib = 3; fib < n; fib++)
+            for (long fib = 3; fib < 44; fib++)
                 sum += S(fib);
             return sum;
         }
@@ -59,7 +61,7 @@ namespace ProjectEuler
         {
             long sum = 0;
             for (long i = 1; i <= n; i++)
-                for (long k = 1; k <= i / 2; k++)        
+                for (long k = 1; k <= i / 2; k++)
                     if (P(i, k))
                         sum++;
         
@@ -75,14 +77,14 @@ namespace ProjectEuler
             else if (partCount == k)
                 return (currentSum == n);
             else
-            {                                
-                foreach (var prime in primes)                    
+            {
+                foreach (var prime in primes)
                 {
                     if (currentSum + (long)prime > n)
                         break;
 
                     if (P_recursive(primes, partCount + 1, n, k, currentSum + (long)prime))
-                        return true;                    
+                        return true;
                 }
                 return false;
             }
