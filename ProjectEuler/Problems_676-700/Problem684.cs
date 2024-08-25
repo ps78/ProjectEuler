4b196e1d7f316b78e3034ded6706e15f9f3db6d2
Pascal Simon <mail@pascalsimon.com>
2024-08-25 12:03:20 +0200

    work on problem 682

--- ProjectEuler/Problems_676-700/Problem684.cs
+++ ProjectEuler/Problems_676-700/Problem684.cs
@@ -9,12 +9,36 @@ namespace ProjectEuler;
 /// </summary>
 public class Problem684 : EulerProblemBase
 {
-    public Problem684() : base(684, "Inverse Digit Sum", 0, 0) { }
-
+    private static readonly Dictionary<ulong, ulong> PowerOfTen;
+    private static Dictionary<ulong, ulong> PowerOfTenSums;
     private static readonly ulong MOD = 1_000_000_007;
+    static Problem684()
+    {
+        PowerOfTen = Enumerable.Range(0, 19).Select(i => ((ulong)i, (ulong)Math.Pow(10, i) % MOD)).ToDictionary();
+        PowerOfTenSums = Enumerable.Range(1, 18).Select(i => ((ulong)i, ulong.Parse(new String('1', i)) % MOD)).ToDictionary();
+        PowerOfTenSums[0] = 0;
+    }
+
+    public Problem684() : base(684, "Inverse Digit Sum", 0, 0) { }
 
     public override long Solve(long n)
     {
+        ulong P = 12;
+        Console.WriteLine($"{PowerOfTenSums[P]}");
+        Console.WriteLine($"{PowerOfTenSumMod(P)}");
+
+        ulong sum = 0;
+        for (ulong k = 0; k < P; k++)
+        {
+            sum = sum + (ulong)Math.Pow(10, k);
+        }
+        Console.WriteLine($"{sum % MOD}");
+
+
+        for (ulong i = 1; i <= 45; i++)
+            Console.WriteLine($"{i} : {S(i)}");
+
+        /*
         ulong sum = 0;
         for (int k = 2; k <= 90; k++)
         {
@@ -24,41 +48,35 @@ public class Problem684 : EulerProblemBase
             sum += Sf;
         }
         return (long)(sum % MOD);
+        */
+        // wrong: 131683844
+        return 0;
     }
-
-    private static Dictionary<ulong, ulong> DecSums = new Dictionary<ulong, ulong>() {
-        {0,  0},
-        {1,  1},
-        {2,  11 },
-        {3,  111 },
-        {4,  1111 },
-        {5,  11111 },
-        {6,  111111 },
-        {7,  1111111 },
-        {8,  11111111 },
-        {9,  111111111 },
-        {10, 1111111111 },
-        {11, 11111111111 },
-        {12, 111111111111 },
-        {13, 1111111111111 },
-        {14, 11111111111111 },
-        {15, 111111111111111 },
-        {16, 1111111111111111 },
-        {17, 11111111111111111 },
-        {18, 111111111111111111 }
-    };
 
     private ulong S(ulong n)
     {
+        // S(45) = 599949
         ulong P = n / 9;
-        ulong decSum = P > 14 ? DecSums[14] : DecSums[P];
-        ulong baseSum = 54 * decSum - 9 * P;
+        ulong idx = Math.Min(P, 14);
+        ulong baseSum = 54 * PowerOfTenSums[idx] - 9 * P;
 
         ulong remSum = 0;
-        ulong dec = P > 14 ? (ulong)1E14 : (ulong)Math.Pow(10, P);
         for (ulong i = 1; i <= n - 9 * P; i++)
-            remSum += ((dec - 1) + i * dec);
+            remSum += ((PowerOfTen[idx] - 1) + i * PowerOfTen[idx]);
 
         return (baseSum + remSum) % MOD;
+    }
+
+    /// <summary>
+    /// Calculates Sum[k=0..P](10^k) % MOD
+    /// </summary>
+    /// <param name="P"></param>
+    /// <returns></returns>
+    private ulong PowerOfTenSumMod(ulong P)
+    {
+        if (P <= 10)
+            return PowerOfTenSums[P];
+        else
+            return (PowerOfTenSumMod(P / 2) * PowerOfTenSumMod(P - P / 2)) % MOD;
     }
 }
